89dc21d5899e7b63e6bdb06b11571482e0e9e567
Vlad Chilom <vchilom6@gmail.com>
2015-07-05 16:41:15 -0600

    Arrow up/down functionality added. Slight css changes.

--- ext/app/scripts/handler.js
+++ ext/app/scripts/handler.js
@@ -19,6 +19,7 @@ for (var i = 0; i < hoverImgChangeId.length; i++) {
 
 document.getElementById("open-up-img").addEventListener("click", function() {
 	if (document.getElementById("song-info-container").style.borderTop != "") {
+		this.setAttribute("src", "./images/open-on.png");
 		document.getElementById("song-info-container").style.borderTop = "";
 		document.getElementById("option-container").style.display = "none";
 		document.getElementById("add-playlist-execute").style.display = "none";
@@ -27,6 +28,7 @@ document.getElementById("open-up-img").addEventListener("click", function() {
 		document.getElementById("playlists-container").style.display = "none";
 		document.getElementById("add-playlist").style.display = "none";
 	} else {
+		this.setAttribute("src", "./images/close-on.png");
 		document.getElementById("song-info-container").style.borderTop = "1px solid rgba(255, 255, 255, 0.3)";
 		document.getElementById("option-container").style.display = "inline";
 		document.getElementById("playlists-container").style.display = "inline";
