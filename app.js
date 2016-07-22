0353b0d819923f65005035374658cdb5a0596b08
Nicholas Bien <nicholasbien@gmail.com>
2016-07-21 21:20:58 -0700

    added mongoose

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/music-player');
+
 var server = http.createServer(app);
 
 require('./settings.js')(app, express);
-require('./routes/index.js')(app);
+require('./routes.js')(app);
 
 server.listen(3000);
 console.log('Listening at localhost:' + 3000);
